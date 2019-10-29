09959bc2599646655f394b646f890cd380dc6de0
thetamicodes <reichold.tamara@gmail.com>
2019-10-29 16:11:34 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const formSelector = document.querySelector('form');
+    formSelector.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const taskName = document.querySelector('input[placeholder="description"]').value;
+      const newToDo = document.createElement('li');
+      newToDo.innerText = `${taskName}`;
+      document.querySelector('#tasks').appendChild(newToDo);
+      // debugger
+    });
+    const deleteButton = document.querySelector("button");
+    deleteButton.addEventListener('click', function(e) {
+      e.preventDefault;
+      const done = document.getElementById('tasks');
+      done.remove('li');
+    })
 });
